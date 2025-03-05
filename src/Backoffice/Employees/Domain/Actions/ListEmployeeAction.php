da1a0e8a75b1172e6a06fcbb5aabbc621857d033
Alan Cohen <alancohen@Alans-MacBook-Pro.local>
2025-03-05 10:44:43 -0300

    fix: task request

--- src/Backoffice/Employees/Domain/Actions/ListEmployeeAction.php
+++ src/Backoffice/Employees/Domain/Actions/ListEmployeeAction.php
@@ -5,7 +5,6 @@ declare(strict_types=1);
 namespace Lightit\Backoffice\Employees\Domain\Actions;
 
 use Illuminate\Database\Eloquent\Collection;
-use Illuminate\Database\Eloquent\Model;
 use Lightit\Backoffice\Employees\Domain\Models\Employee;
 
 class ListEmployeeAction
